28f195fa8d06a1db06d7a873a84a300c1ad0d1b2
m1016m <m1016m@gmail.com>
2018-07-11 13:39:53 +0800

    https://github.com/m1016m/X-village-2018-Exercise.git

--- 0710_homework.py
+++ 0710_homework.py
@@ -11,3 +11,51 @@ while guess != answer:
             print "answer:" , answer
          else:
             print "恭喜答對了"
+print ""
+
+x = 10
+for i in range(1, 10):
+    x -= 1
+    for j in range(1, x ):
+        
+        print "aa", 
+    print ""
+
+num = int(input("請輸入1~100的整數做質數測試 = "))
+if num == 2:                                # 2是質數所以直接輸出
+    print "%d是質數" % num
+else:
+    for n in range(2, num):                 # 用2 .. num-1當除數測試
+        if num % n == 0:                    # 如果整除則不是質數
+            print "%d不是質數" % num
+            break                           # 離開迴圈
+
+    else:                                   # 否則是質數
+        print "%d是質數" % num
+
+
+import math
+def math_h(a,b,c):
+    num=b**2-4*a*c
+    if num < 0:
+        raise ValueError,'it is invalid'
+    kv=math.sqrt(num)
+    return (-b+kv)/a/2,(-b-kv)/a/2
+
+print math_h(1,-2,1)
+print math_h(1,6,8)
+
+
+def myline(startx,starty,endx,endy):
+        for x in range(startx, endx):
+                y=int((x-startx)*(endy-starty)/(endx-startx))+starty
+                print "(x, y) = (%d, %d)" % (x, y)
+ 
+myline(378,279,421,319)
+
+
+
+
+
+
+
