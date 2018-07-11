c92da5dc48fd8344f6b123da9ba0e0d091f34e03
m1016m <m1016m@gmail.com>
2018-07-11 14:36:41 +0800

    add

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,14 +1,7 @@
-import random
-answer = random.randint(0 , 100)# 正確數字
-guess = 0                   # 設定所猜數字的初始值
-while guess != answer:
-         guess = int(input("請猜1-100間的數字 = "))
-         if guess > answer:
-            print "請猜小一點"
-            print "answer:" , answer
-         elif guess < answer:
-            print "請猜大一點"
-            print "answer:" , answer
-         else:
-            print "恭喜答對了"
-print ""
+def f(x):
+    for i in range(2 , x):
+        for j in range(1 , x):
+            print i , "*" , j ,"=" ,i *j,"\t",
+        print "\n"
+
+f(9)
---
+++ Exercise_2.py
@@ -0,0 +1,7 @@
+def f(x,y):
+   a , b = y , x
+   return a,b
+  
+
+a , b =1 ,2
+print f(a, b)
