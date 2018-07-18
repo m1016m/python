5f69bfabe6998c18386c4b8d2b89868bfadef3f0
m1016m <m1016m@gmail.com>
2018-07-18 13:30:20 +0800

    Exercise

--- Lesson 5 - File/Exercise_2.py
+++ Lesson 5 - File/Exercise_2.py
@@ -9,4 +9,4 @@ with open('AQI_20180717105825.csv', 'rb')as f:
     for row in reader:
         if int(row['AQI']) <=10:
          
-           print str(row['County']).decode('string_escape')#解決list印出亂碼的問題
+           print str(row['County']).decode('string_escape'),"空氣最好，AQI 是",row['AQI']#解決list印出亂碼的問題
