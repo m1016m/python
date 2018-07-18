4f402ec47c56c6febf2a829df90f1f1f7e97e3e6
m1016m <m1016m@gmail.com>
2018-07-18 15:52:42 +0800

    Exercise

--- 1.py
+++ 1.py
@@ -4,14 +4,9 @@ import json
 url = 'https://cloud.culture.tw/frontsite/trans/SearchShowAction.do?method=doFindTypeJ&category=5'
 response = requests.get(url)
 
-# get status code
-print('Requests message URL:', response.request.url)
-print('-'*50)
-print('Requests message Method:', response.request.method)
-print('-'*50)
-print('Requests message body:', response.request.body)
+s = json.loads(response.text)
 
 with open('music_show.json', 'w') as outfile:
-    json.dump(response.text, outfile)
+    json.dump(s, outfile)
 
 
