6869f256e74d463c32adbc028ae2c852bfafb036
m1016m <m1016m@gmail.com>
2018-07-26 10:10:57 +0800

    Exercise

--- 2.py
+++
@@ -1,7 +0,0 @@
-import csv
-with open('menu.csv', 'w', encoding='utf-8-sig') as csvfile:
-    filewriter = csv.writer(csvfile, delimiter=',')
-    filewriter.writerow(['品名', '價格'])
-    filewriter.writerow(['餛飩麵', '50'])
-    filewriter.writerow(['蛋花湯', '25'])
-    filewriter.writerow(['可樂', '20'])
