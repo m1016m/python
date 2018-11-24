71823206ef8a5557f5f457d307e1fa3ef9df6fed
m1016m <m1016m@gmail.com>
2018-11-24 17:53:12 +0800

    Update speech.py

--- 嘉義AI/speech/speech.py
+++ 嘉義AI/speech/speech.py
@@ -14,3 +14,16 @@ except sr.UnknownValueError:
     print("Could not understand audio")
 except sr.RequestError as e:
     print("Could not request results; {0}".format(e))
+    
+    
+    
+import speech_recognition as sr
+while True:
+    r=sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Say Something")
+        audio=r.listen(source)
+    try:    
+        print(r.recognize_google(audio),"\n")
+    except:
+        pass
